79fd0185a565b499d0e946438f7115447026625f
jakkra <ijakkra@gmail.com>
2014-11-01 20:27:03 +0100

    test

---
+++ src/Algoritmer.c
@@ -0,0 +1,180 @@
+/*
+ ============================================================================
+ Name        : Algoritmer.c
+ Author      : Jakob Krantz
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+#include "linkedlist.h"
+
+#define min(a, b) (((a) < (b)) ? (a) : (b));
+
+char *getcwd(char *buf, size_t size);
+int ford_fulceson(int source, int destination);
+int bfs(int source, int destination);
+void bfsCut(int source, int goal);
+
+int number_of_nodes = 55;
+void parse();
+double points[280][3];
+int graph[55][55];
+int parent[55];
+int visited[55]; // 1 = true, 0 = false
+int residualGraph[55][55];
+
+int main(int argc, char const *argv[]) {
+	parse();
+	int max_flow = ford_fulceson(0, 54);
+	printf("Max flow = %d\n", max_flow);
+
+	//bfsCut(0, 54);
+	return 0;
+}
+
+int ford_fulceson(int source, int destination) {
+	int u, v;
+	int max_flow = 0;
+	int path_flow = 0;
+	for (int source_vertex = 0; source_vertex < number_of_nodes;
+			source_vertex++) {
+		for (int destination_vertex = 0; destination_vertex < number_of_nodes;
+				destination_vertex++) {
+			residualGraph[source_vertex][destination_vertex] =
+					graph[source_vertex][destination_vertex];
+		}
+	}
+	while (bfs(source, destination)) {
+		puts("Stuck");
+		path_flow = INT_FAST16_MAX;
+		for (v = destination; v != source; v = parent[v]) {
+			u = parent[v];
+			path_flow = min(path_flow, residualGraph[u][v])
+			;
+		}
+		for (v = destination; v != source; v = parent[v]) {
+			u = parent[v];
+			residualGraph[u][v] -= path_flow;
+			residualGraph[v][u] += path_flow;
+		}
+		max_flow += path_flow;
+
+	}
+	return max_flow;
+
+}
+
+void bfsCut(int source, int goal) {
+	int element;
+	for (int vertex = 0; vertex < number_of_nodes; vertex++) {
+		parent[vertex] = -1;
+		visited[vertex] = 0;
+	}
+	add_end(source);
+	parent[source] = -1;
+	visited[source] = 1;
+	while (!is_empty()) {
+		puts("Stuck inside bfs()");
+
+		element = remove_first();
+		for (int index = 0; index < number_of_nodes; index++) {
+
+			if (residualGraph[element][index] > 0 && !visited[index]) {
+				parent[index] = element;
+				add_beginning(index);
+				visited[index] = 1;
+			}
+		}
+	}
+	/*for (int i = 0; i < number_of_nodes; i++) {
+	 for (int j = 0; j < number_of_nodes; j++) {
+	 if (visited[i] && !visited[j] && (graph[i][j] > 0)) {
+	 puts(i + " " + j + " " + graph[i][j]);
+	 }
+	 }
+
+	 }
+	 */
+}
+
+int bfs(int source, int goal) {
+	int path_found = 0;
+	int element = 0;
+
+	for (int vertex = 0; vertex < number_of_nodes; vertex++) {
+		parent[vertex] = -1;
+		visited[vertex] = 0;
+	}
+	add_end(source);
+	parent[source] = -1;
+	visited[source] = 1;
+
+	while (!is_empty()) {
+		element = remove_first();
+		for (int index = 0; index < number_of_nodes; index++) {
+			if (graph[element][index] > 0 && !visited[index]) {
+				parent[index] = element;
+				add_end(index);
+				visited[index] = 1;
+
+			}
+		}
+	}
+	if (visited[goal]) {
+		path_found = 1;
+	}
+	return path_found;
+
+}
+
+void parse() {
+	char working_directory[1024];
+	if (getcwd(working_directory, sizeof(working_directory)) != NULL) {
+		fprintf(stdout, "Current working dir: %s\n", working_directory);
+	} else {
+		perror("getcwd() error");
+	}
+
+	static const char filename[] = "/rail.txt";
+
+	char *full_path;
+	full_path = malloc(strlen(working_directory) + strlen(filename) + 1);
+	full_path[0] = '\0';
+	strcat(full_path, working_directory);
+	strcat(full_path, filename);
+	printf("%s\n", full_path);
+
+	FILE *file = fopen(full_path, "r");
+	if (file != NULL) {
+
+		char line[128]; /* or other suitable maximum line size */
+
+		/*Skip untill we se flag "DESTINATIONS"*/
+		while (fgets(line, sizeof line, file) != NULL
+				&& strstr(line, "DESTINATIONS") == NULL) {
+		}
+		/* read the input */
+		char *splitted_value;
+		while (fgets(line, sizeof line, file) != NULL
+				&& strstr(line, "EOF") == NULL) {
+
+			int column = atoi(splitted_value = strtok(line, " "));
+			int row = atoi(splitted_value = strtok(NULL, " "));
+			int weight =
+					atoi(splitted_value = strtok(NULL, " ")) != -1 ?
+							atoi(splitted_value) : 1000;
+			graph[column][row] = weight;
+
+		}
+		fclose(file);
+	} else {
+		perror(filename); /* why didn't the file open? */
+	}
+}
---
+++ src/linkedlist.c
@@ -0,0 +1,98 @@
+/*
+ * linkedlist.c
+ *
+ *  Created on: 1 nov 2014
+ *      Author: krantz
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include "linkedlist.h"
+
+struct node
+{
+    int data;
+    struct node *next;
+} *head, *var, *trav;
+
+/*Adds a new node as head, with specified value*/
+void add_beginning(int val)
+{
+
+    var = (struct node *)malloc(sizeof(struct node));
+    var -> data = val;
+    if (head == NULL)
+    {
+        head = var;
+        head -> next = NULL;
+    }
+    else
+    {
+        var -> next = head;
+        head = var;
+    }
+
+   // printf("Data added at beginning %d\n", head -> data);
+}
+/*Adds a new node with specified value at the end of the list*/
+void add_end(int val)
+{
+    struct node *temp;
+    temp = head;
+    var = (struct  node *)malloc(sizeof(struct  node));
+    var -> data = val;
+
+    if (head == NULL)
+    {
+        head = var;
+        head -> next = NULL;
+    }
+    else
+    {
+        while (temp -> next != NULL)
+        {
+            temp = temp-> next;
+        }
+        var -> next = NULL;
+        temp -> next = var;
+    }
+   // printf("new node added at end %d\n", var -> data);
+
+
+
+}
+/* Removes the head element*/
+int remove_first()
+{
+    if (head != NULL)
+    {
+        struct node *temp;
+        temp = head;
+        int tempVal = head -> data;
+        head = temp -> next;
+        //printf("head removed %d\n", temp -> data);
+        free(temp);
+        return tempVal;
+    }
+  //  printf("List is empty, returns%d\n", -1);
+    return -1;
+}
+/*Prints every value in the linked list*/
+void print()
+{
+    struct node *temp;
+    temp = head;
+    while (temp -> next != NULL)
+    {
+       // printf("%d\n", temp -> data);
+        temp = temp -> next;
+    }
+    //printf("%d\n", temp -> data);
+
+}
+
+/* Returns 1 if the linked list is empty, 0 of not empty*/
+int is_empty()
+{
+    return head != NULL ? 0 : 1;
+}
